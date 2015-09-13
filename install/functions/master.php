0c243bb26e08cba1b50923c168750fdfdcfa019b
dodiaraculus <dodiaraculus@outlook.com>
2015-09-13 01:20:24 +0000

    Fixed Authorization Security and Addeded Levels

--- install/functions/master.php
+++ install/functions/master.php
@@ -6,14 +6,14 @@ $r1='/[A-Z]/';  //Uppercase
     $r3='/[!@#$%^&*()-_=+{};:,<.>]/';
     $r4='/[0-9]/';  //numbers
 
-if (!preg_match_all($r4, odbc_result($rs,$passworda))) {
+if (!preg_match($r4, $passworda)) {
     return false;
 }
 
-if (!preg_match_all($r2, odbc_result($rs,$passworda))) {
+if (!preg_match($r2, $passworda)) {
     return false;
 }
-if (!preg_match_all($r1, odbc_result($rs,$passworda))) {
+if (!preg_match($r1,$passworda)) {
     return false;
 }
 return true;
